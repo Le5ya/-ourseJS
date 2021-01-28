896e380e3a79d48153b912ae08101bed5c0d53ec
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-01-28 18:36:08 +0300

    notifications

--- script/script.js
+++ script/script.js
@@ -3,4 +3,6 @@ let money = document.querySelector('.money'),
     add__expenses = document.querySelector('.add__expenses'), 
     deposit = document.querySelector('.deposit'), 
     mission = document.querySelector('.mission'), 
-    period = document.querySelector('.period'), 
+    period = document.querySelector('.period');
+    alert('Start first lesson'); 
+    console.log('variabl JS');
