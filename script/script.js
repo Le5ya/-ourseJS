ddd3c92b4218a5a99ab7be0089f05323157ef8f4
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-01-29 15:16:23 +0300

    less-1 obligatory task

--- script/script.js
+++ script/script.js
@@ -1,8 +1,30 @@
-const money = document.querySelector('.money'), 
-      incom = document.querySelector('.incom'), 
-      addExpenses = document.querySelector('.add__expenses'), 
-      deposit = document.querySelector('.deposit'), 
-      mission = document.querySelector('.mission'), 
-      period = document.querySelector('.period');
-alert('Start first lesson'); 
-console.log('variabl JS');
+// const money = document.querySelector('.money'), 
+//       incom = document.querySelector('.incom'), 
+//       addExpenses = document.querySelector('.add__expenses'), 
+//       deposit = document.querySelector('.deposit'), 
+//       mission = document.querySelector('.mission'), 
+//       period = document.querySelector('.period');
+// alert('Start first lesson'); 
+// console.log('variabl JS');
+
+// lesson02
+
+let money = 123456;
+console.log(typeof money);
+let income = 'freelance';
+console.log(typeof income);
+let addExpenses = ('internet'+','+ 'taxi'+','+ 'communal pay');
+console.log(addExpenses.length);
+let deposit = true;
+console.log(typeof deposit);
+let mission = 100000;
+let period = 7 ;
+console.log('Период равен' + ' '+period +' '+ 'месяцев');
+addExpenses.toLowerCase().split(',', Array);
+console.log(addExpenses.toLowerCase().split(','));
+
+let budgetDay;
+    budgetDay = 1500;
+    console.log('budgetDay'+' '+'='+' '+budgetDay)
+
+
