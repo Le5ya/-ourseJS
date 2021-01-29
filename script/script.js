3e80aae00f5ea9b7edd928ca08685b9c826aa24d
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-01-29 13:04:05 +0300

    correction

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
-let money = document.querySelector('.money'), 
-    incom = document.querySelector('.incom'), 
-    addExpenses = document.querySelector('.add__expenses'), 
-    deposit = document.querySelector('.deposit'), 
-    mission = document.querySelector('.mission'), 
-    period = document.querySelector('.period');
-    alert('Start first lesson'); 
-    console.log('variabl JS');
+const money = document.querySelector('.money'), 
+      incom = document.querySelector('.incom'), 
+      addExpenses = document.querySelector('.add__expenses'), 
+      deposit = document.querySelector('.deposit'), 
+      mission = document.querySelector('.mission'), 
+      period = document.querySelector('.period');
+alert('Start first lesson'); 
+console.log('variabl JS');
