c2e73a76a8cdea2779f44f7fb4b5767fef4ba51f
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-02-01 18:26:43 +0300

    lesson03 Динамическая типизация данных

--- script/script.js
+++ script/script.js
@@ -25,6 +25,51 @@ console.log(addExpenses.toLowerCase().split(','));
 
 let budgetDay;
     budgetDay = 1500;
-    console.log('budgetDay'+' '+'='+' '+budgetDay)
+    console.log('budgetDay'+' '+'='+' '+budgetDay);
+
+'use strict';
+
+let incom = prompt('Ваш месячный доход?');
+console.log(incom);
+
+let addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую');
+
+let deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов', 'foods');
+console.log(expenses1)
+let amount1 = prompt('Во сколько это обойдётся?');
+console.log(amount1);
+let expenses2 = prompt('Введите обязательную статью расходов');
+console.log(expenses2)
+let amount2 = prompt('Во сколько это обойдётся?');
+console.log(amount2);
+
+let budgetMonth = incom - amount1 - amount2;
+console.log(budgetMonth);
+
+let mission = prompt('Цель:', 99999.5);
+console.log("Цель заработать" +" " + mission + " " + "рублей");
+let period = Math.ceil(mission / budgetMonth );
+console.log("Период равен"+" " + period +" " + "месяцев");
+
+let budgetDay = budgetMonth / 30;
+console.log(Math.floor(budgetDay));
+
+if (budgetDay > 1200) {
+  alert("У вас высокий уровень дохода");
+} else {
+ if (budgetDay > 600 && budgetDay <= 1200) {
+   alert("У вас средний уровень дохода");
+ } else {
+  if (budgetDay >= 0 && budgetDay < 600) {
+   alert("К сожалению у вас уровень дохода ниже среднего");
+ } else {
+   if (budgetDay < 0) {
+  alert("Что то пошло не так");
+}
+ }
+ } 
+} 
 
 
