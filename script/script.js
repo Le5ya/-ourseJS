57b54d9fb1e8663d946ebe30ec55b8af165a5827
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-04-14 11:11:38 +0300

    comeon

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -11,36 +10,18 @@ let money,
  mission = +prompt('Цель:', 99999.5),
  period = 3;
 
-// let period = Math.ceil(mission / budgetMonth );
+
 
 
 let start = function(){
 money = +prompt('Ваш месячный доход?');
 
 while (!isNumber(money)) {
-	money = +prompt('Ваш месячный доход?');
+  money = +prompt('Ваш месячный доход?');
    }
 };
 
 start();
-=======
-
-
-'use strict';
-
-let money = +prompt('Ваш месячный доход?', 50000);
-let income = 'freelance';
-let addExpenses = prompt('Перечислите возможные расходы за расчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let expenses1 = prompt('Введите обязательную статью расходов', 'foods');
-let amount1 = +prompt('Во сколько это обойдётся?');
-let expenses2 = prompt('Введите обязательную статью расходов');
-let amount2 = +prompt('Во сколько это обойдётся?');
-let accumulatedMonth = getAccumulatedMonth();
-let mission = +prompt('Цель:', 99999.5);
-let period = Math.ceil(mission / accumulatedMonth );
-let budgetDay = accumulatedMonth / 30;
->>>>>>> 6449faeb1c1657934aaff2886e62b42e4b03761b
 
 
 let showTypeOf = function (data) {
@@ -51,7 +32,6 @@ showTypeOf(income);
 showTypeOf(deposit);
 console.log(income.length );
 
-<<<<<<< HEAD
 let expenses = [];
 
 console.log(addExpenses.toLowerCase().split(','));
@@ -63,8 +43,8 @@ console.log(addExpenses.toLowerCase().split(','));
 let getExpensesMonth = function () {
  let sum = 0;
  for (let i=0; i<2; i++){
- 	if(i===0){
- 		expenses[i] = prompt('Введите обязательную статью расходов', 'foods');
+  if(i===0){
+    expenses[i] = prompt('Введите обязательную статью расходов', 'foods');
     sum+= +prompt('Во сколько это обойдётся?');
    }
  console.log(expenses);
@@ -82,49 +62,16 @@ getExpensesMonth();
    return money - expensesMonth;
  }
  let budgetMonth = getBudgetMonth();
+     period = Math.ceil(mission / budgetMonth );
 
-let getTargetMonth = function(targetMonth){
- targetMonth =  mission / budgetMonth;
- return targetMonth;
-}
-getTargetMonth();
 
-console.log('Цель будет достигнута через ' + getTargetMonth());
+console.log('Цель будет достигнута через ' + period + ' месяцев');
 
 let budgetDay = budgetMonth / 30;
 
 
 let getStatusIncome = function () {
-	
-=======
-let exp = [];
-exp += addExpenses;
-console.log(exp);
-
-let getExpensesMonth = function (amount) {
-  amount = (amount1 + amount2);
-  return amount; 
-}
-console.log(getExpensesMonth());
-
-function getAccumulatedMonth(acc) {
-  acc = money - amount1 - amount2;
-  return acc;
-}
-console.log(getAccumulatedMonth());
-
-
-
-let getTargetMonth = function(targetMonth){
-targetMonth =  mission / accumulatedMonth;
- return targetMonth;
-}
-getTargetMonth();
-console.log(getTargetMonth());
-
-
-let getStatusIncome = function () {
->>>>>>> 6449faeb1c1657934aaff2886e62b42e4b03761b
+  
  if (budgetDay > 1200) {
   return ("У вас высокий уровень дохода");
 } else if (budgetDay > 600 && budgetDay <= 1200) {
@@ -136,10 +83,4 @@ let getStatusIncome = function () {
        }
      };
    console.log(getStatusIncome()); 
-<<<<<<< HEAD
-
-=======
- 
- 
->>>>>>> 6449faeb1c1657934aaff2886e62b42e4b03761b
 
